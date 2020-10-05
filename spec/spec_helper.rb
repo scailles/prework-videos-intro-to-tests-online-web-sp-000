c52f109406492c81dc01a80a278ed9e7a45fc8a5
Shaina C <scailles23@gmail.com>
2020-10-05 23:07:32 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -80,10 +80,6 @@ RSpec.configure do |config|
 end
 
 
-ruby describe "conversions" do describe '#ounces_to_grams' do it 'given zero, returns 0.0' end end
-
-
-ruby describe "conversions" do describe '#ounces_to_grams' do it 'given zero, returns 0.0' do grams = ounces_to_grams(0) expect(grams).to eq(0.0) end end end
 
 
 
